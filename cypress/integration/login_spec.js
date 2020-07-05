d94ada772bfb9dcfac45c97616e236d8246bf88f
Alexander Bennell <alexanderdbennell@gmail.com>
2020-07-05 14:14:33 +0100

    Add additional login tests to check different states of user data entry

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -11,22 +11,36 @@ describe('Login Page tests', function() {
         cy.contains('Don\'t have an account? Sign Up').should('have.attr' ,'href', '/signup')
     })
 
-    it('should allow a valid user to login', function () {
-       
-        //Arrange
-       
+
+    it('should require a username', function() {
         //Act
-        cy.get('#username').type('Katharina_Bernier')
-        cy.get('#password').type('s3cret')
+        cy.get('#password').type('password')
+
+
+        //Assert
+        cy.get('form').contains('Sign In').should('be.disabled')
+    })
+
+    it('should require a password', function() {
+        //Act
+        cy.get('#username').type('root')
+
+        //Assert
+        cy.get('form').contains('Sign In').should('be.disabled')
+    })
+
+    it('should require a username and password', function() {
+        //Act
         cy.get('form').contains('Sign In').click()
 
         //Assert
-        cy.get('span').contains('Logout').should('be.visible')
+        cy.get('#username-helper-text').contains('Username is required')
+
+   
     })
 
     it('shouldn\'t allow an invalid user to login', function () {
        
-        //Arrange
 
         //Act
         cy.get('#username').type('root')
@@ -36,5 +50,19 @@ describe('Login Page tests', function() {
         //Assert
         cy.get('.MuiAlert-message').should('contain', 'Username or password is invalid')
     })
+
+    it('should allow a valid user to login', function () {
+       
+       
+        //Act
+        cy.get('#username').type('Katharina_Bernier')
+        cy.get('#password').type('s3cret')
+        cy.get('form').contains('Sign In').click()
+
+        //Assert
+        cy.contains('Logout').should('be.visible')
+    })
+
+
 
 })
