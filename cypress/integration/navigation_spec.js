70f4e7d3650e46124a11de1a1b4916e31dc060c4
Alexander Bennell <alexanderdbennell@gmail.com>
2020-07-05 16:29:23 +0100

    Change global login mechanism to login via api and update localStorage + add additional navigation UI tests

--- cypress/integration/navigation_spec.js
+++ cypress/integration/navigation_spec.js
@@ -1,10 +1,37 @@
 describe('Navigation tests', function() {
     beforeEach(() => {
         cy.login()
+        cy.visit('http://localhost:3000')
+    })
+
+    it('should display the user\'s profile picture', function() {
+        cy.get('img.MuiAvatar-img').should('have.attr','src','https://api.adorable.io/avatars/128/t45AiwidW.png')
     })
     
-    it('Should log the user out successfully', function () {
+    it('should display the user\'s full name', function() {
+        cy.get('h6.MuiTypography-subtitle1').should('contain', 'Edgar J')
+    })
+
+    it('should display the user\'s username', function () {
+        cy.get('h6.MuiTypography-subtitle2').should('contain', '@Katharina_Bernier')
+    })
+    
+    it('should have a link to the My Account page', function() {
+        cy.contains('My Account').should('have.attr','href', '/user/settings')
+    })
+
+    it('should have a link to the Bank Accounts page', function() {
+        cy.contains('Bank Accounts').should('have.attr','href', '/bankaccounts')
+    })
+
+    it('should have a link to the Notifications page', function() {
+        cy.contains('Notifications').should('have.attr','href', '/notifications')
+    })
+
+    it('should log the user out successfully', function () {
         cy.contains('Logout').click()
         cy.contains('h1', 'Sign in')
     })
+
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,12 +27,22 @@
 //Add a new library to handle modification of local storage
 import "cypress-localstorage-commands"
 
+
 //This will give me a new login command I can use in any test which requires the user to be authenticated.
 Cypress.Commands.add('login', () => {
+        
+        
+        cy.request({
+                method: 'POST',
+                url: 'http://localhost:3001/login',
+                body: {
+                        type: "LOGIN",
+                        username: 'Katharina_Bernier',
+                        password: 's3cret'
+                }
 
-        cy.visit('http://localhost:3000/signin')
-        cy.get('#username').type('Katharina_Bernier')
-        cy.get('#password').type('s3cret')
-        cy.get('form').contains('Sign In').click()
+        }).then((resp) => {
+                 cy.setLocalStorage('authState','{"actions":[{"type":"xstate.stop","activity":{"id":"performLogin","src":"performLogin","onDone":{"target":"authorized","actions":"onSuccess"},"onError":{"target":"unauthorized","actions":"onError"},"type":"xstate.invoke"}},{"type":"redirectHomeAfterLogin"}],"activities":{"performLogin":false},"meta":{},"events":[],"value":"authorized","context":{"user":{"id":"t45AiwidW","uuid":"6383f84e-b511-44c5-a835-3ece1d781fa8","firstName":"Edgar","lastName":"Johns","username":"Katharina_Bernier","password":"$2a$10$5PXHGtcsckWtAprT5/JmluhR13f16BL8SIGhvAKNP.Dhxkt69FfzW","email":"Norene39@yahoo.com","phoneNumber":"625-316-9882","avatar":"https://api.adorable.io/avatars/128/t45AiwidW.png","defaultPrivacyLevel":"public","balance":168137,"createdAt":"2019-08-27T23:47:05.637Z","modifiedAt":"2020-05-21T11:02:22.857Z"}},"_event":{"name":"done.invoke.performLogin","data":{"type":"done.invoke.performLogin","data":{"user":{"id":"t45AiwidW","uuid":"6383f84e-b511-44c5-a835-3ece1d781fa8","firstName":"Edgar","lastName":"Johns","username":"Katharina_Bernier","password":"$2a$10$5PXHGtcsckWtAprT5/JmluhR13f16BL8SIGhvAKNP.Dhxkt69FfzW","email":"Norene39@yahoo.com","phoneNumber":"625-316-9882","avatar":"https://api.adorable.io/avatars/128/t45AiwidW.png","defaultPrivacyLevel":"public","balance":168137,"createdAt":"2019-08-27T23:47:05.637Z","modifiedAt":"2020-05-21T11:02:22.857Z"}}},"$$type":"scxml","type":"external","origin":"performLogin"},"_sessionid":"x:0","event":{"type":"done.invoke.performLogin","data":{"user":{"id":"t45AiwidW","uuid":"6383f84e-b511-44c5-a835-3ece1d781fa8","firstName":"Edgar","lastName":"Johns","username":"Katharina_Bernier","password":"$2a$10$5PXHGtcsckWtAprT5/JmluhR13f16BL8SIGhvAKNP.Dhxkt69FfzW","email":"Norene39@yahoo.com","phoneNumber":"625-316-9882","avatar":"https://api.adorable.io/avatars/128/t45AiwidW.png","defaultPrivacyLevel":"public","balance":168137,"createdAt":"2019-08-27T23:47:05.637Z","modifiedAt":"2020-05-21T11:02:22.857Z"}}},"historyValue":{"current":"authorized","states":{}},"history":{"actions":[{"type":"xstate.start","activity":{"id":"performLogin","src":"performLogin","onDone":{"target":"authorized","actions":"onSuccess"},"onError":{"target":"unauthorized","actions":"onError"},"type":"xstate.invoke"}}],"activities":{"performLogin":{"type":"xstate.start","activity":{"id":"performLogin","src":"performLogin","onDone":{"target":"authorized","actions":"onSuccess"},"onError":{"target":"unauthorized","actions":"onError"},"type":"xstate.invoke"}}},"meta":{},"events":[],"value":"loading","context":{},"_event":{"name":"LOGIN","data":{"type":"LOGIN","username":"Katharina_Bernier","password":"s3cret"},"$$type":"scxml","type":"external"},"_sessionid":"x:0","event":{"type":"LOGIN","username":"Katharina_Bernier","password":"s3cret"},"historyValue":{"current":"loading","states":{}},"children":{},"done":false,"changed":true},"children":{},"done":false,"changed":true}')
+        })
 
 })
